c7453aca980b2e9353d2bb30d1506d531ae9d746
premnath <premnathkulal1998@gmail.com>
2019-02-24 06:54:18 +0530

    3

---
+++ app/src/main/java/com/example/sh_enterprises/categories.java
@@ -0,0 +1,13 @@
+package com.example.sh_enterprises;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class categories extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_categories);
+    }
+}
